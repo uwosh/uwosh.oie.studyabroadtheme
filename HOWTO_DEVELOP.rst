e1285485a3538cd3696ce3e00ad66067cd79b15d
lucid-0 <kylearthurs21@gmail.com>
2019-07-03 10:38:51 -0500

    Fix angular not defined, jq not defined, 404, etc

--- HOWTO_DEVELOP.rst
+++ HOWTO_DEVELOP.rst
@@ -14,7 +14,7 @@ After executing these commands you can run grunt on the package root folder to w
 
 This will make sure that the many .less files are compiled to .css on the fly and then served up from the theme.
 
-You can also use this command to automaticly reload the browser after changes::
+You can also use this command to automatically reload the browser after changes::
 
     $ grunt plone-bsync
 
@@ -28,12 +28,12 @@ or use the helper script from buildout::
 
     $ ./bin/grunt-task-compile
 
-Actually on a buildout run buildout will run the npm-install and the grunt-task-compile script for you. So you only need the commands above while developing gin the filesystem.
+Actually on a buildout run buildout will run the npm-install and the grunt-task-compile script for you. So you only need the commands above while developing in the filesystem.
 
 Note
 ----
 
-- If you are make changes TTW, make sure that Development Mode is enabled by going to the `Site Setup` > `Resource Registries`
+- If you are making changes TTW, make sure that Development Mode is enabled by going to the `Site Setup` > `Resource Registries`
   and check the `Development Mode` checkbox, this ensures that your changes show up and aren't cached.
 - this theme is using a Grunt setup to compile the resources. The grunt setup is outside of the theme folder in the package root. If you only have a Zip-file version of this theme, have a look at the example theme plonetheme.tango (github.com/collective) for the Gruntfile.js and package.json to setup your own Grunt above your theme folder.
 - You can delete the "custom-theme-marker" in the index.html and in custom.less
--- src/uwosh/oie/studyabroadtheme/theme/js/plone-rwd-nav.js
+++ src/uwosh/oie/studyabroadtheme/theme/js/plone-rwd-nav.js
@@ -1,4 +1,4 @@
-/* Sample scripts for RWD nav patterns 
+/* Sample scripts for RWD nav patterns
    (c) 2012 Maggie Wachs, Filament Group, Inc - http://filamentgroup.com/examples/rwd-nav-patterns/GPL-LICENSE.txt
    Last updated: March 2012
    Dependencies: jQuery
@@ -10,38 +10,38 @@ function below_610(){
       if(window.location.hash != '#portal-globalnav') {
         if ($(window).width() < 610) {
             var showNavigationLabel = _('Navigation');
-            var hideNavigationLabel = _('Hide Navigation');  
-            
+            var hideNavigationLabel = _('Hide Navigation');
+
             //$("#portal-globalnav").prepend('<li class="navigation-pulldown">Navigation</li>');
-            
-            // always show right Navigation label for button, even in rare situations: 
-            // e.g. expand the mobile menu, enlarge the browser so the mobile menu disappears, 
+
+            // always show right Navigation label for button, even in rare situations:
+            // e.g. expand the mobile menu, enlarge the browser so the mobile menu disappears,
             //      make the browser smaller and see the right label with the expanded menu again
             var initialNavigationLabel = showNavigationLabel;
             if ($('.nav-primary').hasClass('expanded')) {
                    initialNavigationLabel = hideNavigationLabel;
-            }                   
+            }
             $("#portal-globalnav").prepend('<button type="button" class="navigation-button">' + initialNavigationLabel + '</button>');
             $('#portal-top').addClass('nav-menu');
             $(".nav-primary").prepend('<span class="nav-section" />');
             // toggle the menu items' visiblity. it gives the parent the class .expanded
             $('.nav-primary').find('span.nav-section').bind('click focus', function(){
                 $(this).parent().toggleClass('expanded');
-               // always show right Navigation label for button, even in rare situations...               
+               // always show right Navigation label for button, even in rare situations...
                 if ($('.nav-primary').hasClass('expanded')) {
                     $(this).parent().find('button.navigation-button').text(hideNavigationLabel);
                 } else {
-                    $(this).parent().find('button.navigation-button').text(showNavigationLabel); 
-                }                 
+                    $(this).parent().find('button.navigation-button').text(showNavigationLabel);
+                }
                 });
             $('button.navigation-button').bind('click', function() {
                $('.nav-primary').toggleClass('expanded');
-               // always show right Navigation label for button, even in rare situations...               
+               // always show right Navigation label for button, even in rare situations...
                if ($('.nav-primary').hasClass('expanded')) {
                    $(this).text(hideNavigationLabel);
                } else {
-                   $(this).text(showNavigationLabel); 
-               }                 
+                   $(this).text(showNavigationLabel);
+               }
                });
         }
      }
@@ -55,23 +55,23 @@ $(document).ready(function() {
         }
         $(window).resize(function() {
             // if window is smaller than 610px put in the extra li
-            if ($(window).width() <= 610) { 
+            if ($(window).width() <= 610) {
                  // check to see if we have this particular li.. if not add it
                  if ( $(".navigation-button").length == 0 ) {
-                   below_610(); 
+                   below_610();
                  }
-             } 
+             }
             // if window is bigger take away li
             if ($(window).width() > 610) {
                  // check and see if .navigation-button is there.. if so remove it
                  if ( $(".navigation-button").length > 0 ) {
-                     $('#portal-top').removeClass('nav-menu'); 
+                     $('#portal-top').removeClass('nav-menu');
                      $('.navigation-button').remove();
                      $('.nav-section').remove();
                  }
             }
 
         });
-   
+
 
 });
