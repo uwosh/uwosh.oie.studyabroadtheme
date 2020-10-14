971005170cd47e353408ce0176edc449a2861612
Brian Duncan <brian.duncan@wildcardcorp.com>
2020-10-14 09:43:50 -0500

    travis changes

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing',
             'plone.app.contenttypes',
             'plone.app.robotframework[debug]',
         ],
