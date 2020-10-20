fefd9c3f2c2aa824cbfeef00fcb17fd5f00b7f47
lucid-0 <kylearthurs21@gmail.com>
2020-10-20 09:48:20 -0500

    Preparing release 1.0.6

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.6 (unreleased)
+1.0.6 (2020-10-20)
 ------------------
 
 - setuptools and Travis fixes
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='uwosh.oie.studyabroadtheme',
-    version='1.0.6.dev0',
+    version='1.0.6',
     description="Plone theme for UW Oshkosh Office of International Education",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
